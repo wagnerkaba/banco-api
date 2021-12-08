f237d4001b45355a7fe5d7da412d21165c234f8d
wagnerkaba <bom.link+github@gmail.com>
2021-12-08 15:16:32 -0300

    Correção de bugs

--- src/main/java/com/wagner/bancoapi/service/ClienteService.java
+++ src/main/java/com/wagner/bancoapi/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.wagner.bancoapi.service;
 import com.wagner.bancoapi.dto.response.MessageResponseDTO;
 import com.wagner.bancoapi.entity.Cliente;
 import com.wagner.bancoapi.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ClienteService {
 
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteService(ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+
 
     public MessageResponseDTO createCliente(@RequestBody Cliente cliente){
         Cliente savedCliente = clienteRepository.save(cliente);
